351533dd4602b21df839d7795b6bbbecb5c99d09
xiluna <xilaluna2@gmail.com>
2020-10-21 19:41:50 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 
 # TODO: Follow the assignment instructions to complete the required routes!
 # (And make sure to delete this TODO message when you're done!)
+
+from flask import Flask
+
+app = Flask(__name__)
